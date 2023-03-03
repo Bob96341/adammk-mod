af15c92bcdb1b8ec2daeabaf4b5e9c1dd712a41c
bob <sergeianimegod@gmail.com>
2023-03-03 17:23:43 +0100

    adamium blocks

--- src/main/java/net/tylericepack/adammkmod/AdammkMod.java
+++ src/main/java/net/tylericepack/adammkmod/AdammkMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.tylericepack.adammkmod.block.ModBlocks;
 import net.tylericepack.adammkmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -19,6 +20,7 @@ public class AdammkMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
